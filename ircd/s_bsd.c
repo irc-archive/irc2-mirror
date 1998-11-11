61d5218e6ed1c91527819b9ecb4bf3bee679f97c
Jarkko Oikarinen <jto@tolsun.oulu.fi>
1998-11-12 09:00:00 +1000

    irc2.10.1

--- ircd/s_bsd.c
+++ ircd/s_bsd.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_bsd.c,v 1.42 1998/10/27 10:01:28 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_bsd.c,v 1.44 1998/11/12 11:46:36 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -2414,7 +2414,8 @@ int	*lenp;
 		sizeof(struct in_addr));
 	bcopy((char *)&aconf->ipnum, (char *)&cptr->ip,
 		sizeof(struct in_addr));
-	server.sin_port = htons((aconf->port > 0) ? aconf->port : portnum);
+	cptr->port = (aconf->port > 0) ? aconf->port : portnum;
+	server.sin_port = htons(cptr->port);
 	/*
 	 * Look for a duplicate IP#,port pair among already open connections
 	 * (This caters for unestablished connections).
@@ -2766,10 +2767,10 @@ aConfItem *aconf;
 	pi.pi_seq = cp->lseq++;
 	cp->seq++;
 	/*
-	 * Only recognise stats from the last 10 minutes as significant...
+	 * Only recognise stats from the last 20 minutes as significant...
 	 * Try and fake sliding along a "window" here.
 	 */
-	if (cp->seq * aconf->class->conFreq > 600)
+	if (cp->seq > 1 && cp->seq * aconf->class->conFreq > 1200)
 	    {
 		if (cp->recv)
 		    {
